c7b6f65ee0c289009f53da6d8794d2b0eea72a70
Rasul <ddzon4553@gmail.com>
2026-08-17 21:55:34 +0300

    исправил форматирования и задание про скорость света

--- homework-7.js
+++ homework-7.js
@@ -1,45 +1,37 @@
-function weather(city, temperature,) // Функция для определения темпераутуры в городе
-{
-    console.log(`В городе ${city} сейчас ${temperature} градусов`);
+function weather(city, temperature) {
+  // Функция для определения темпераутуры в городе
+  console.log(`В городе ${city} сейчас ${temperature} градусов`);
 }
-weather('Ачхой-Мартан', 30)
+weather("Ачхой-Мартан", 30);
 
-let speed = 299792458; // Скорость света в м/с
+function checkspeed(speed) {
+  if (checkspeed > 299792458) {
+    console.log("Сверхсветовая скорость");
+  } else if (checkspeed == 299792458) {
+    console.log("Световая скорость");
+  } else {
+    console.log("Субсветовая скорость");
+  }
+}
 
-if(speed > 299792458)
-{
-    console.log('Сверхсветовая скорость');
-}
-else if(speed == 299792458)
-{
-    console.log('Световая скорость');
-}
-else{
-       console.log('Субсветовая скорость');
-    }
+checkspeed(299792458);
 
 let price = 2500;
 let name = "Футбольный мяч";
-function product(budget) 
-{
-    if(budget >= price)
-    {
-        console.log(`${name} приобретён. Спасибо за покупку!`);
-    }
-    else
-    {
-        console.log(`Вам не хватает ${price - budget}$, пополните баланс`);
-    }
+function product(budget) {
+  if (budget >= price) {
+    console.log(`${name} приобретён. Спасибо за покупку!`);
+  } else {
+    console.log(`Вам не хватает ${price - budget}$, пополните баланс`);
+  }
 }
 
-product(3000)
+product(3000);
 
-function myfunction(a, b) // Функция для сложения двух чисел
-{
-    return 0;
+function myfunction(a, b) {
+  return 0;
 }
 
 let strength = 10;
 let agility = 5;
 let intelligence = 8;
-
